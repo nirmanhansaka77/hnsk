ae91fb4566422e286001a94f9473bfc161d193cb
Hnsk23 <hnsk237788@gmail.com>
2026-02-17 16:33:47 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "TNFUzLrQ#ita1AYTmJfaw7ep_nBIWaYBacBmcGriwQg_uPKZ-V98",
+SESSION_ID: process.env.SESSION_ID || "zIVkgAIJ#FXKMBZDSchrSvc_pHF2aBHHLuN_tCbiNclEoGt9fVnU",
 ALIVE_IMG: process.env.ALIVE_IMG || "https://github.com/Hnsk23/hnsk/blob/main/images/HANSA%20md.png?raw=true",
 ALIVE_MSG: process.env.ALIVE_MSG || "*Hello👋 HANSA-MD Is Alive Now😍*",
 BOT_OWNER: '94768546691',  // Replace with the owner's phone number
